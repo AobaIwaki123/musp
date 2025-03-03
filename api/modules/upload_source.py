bc8a57ad20cc60508f573b9232335b1dd9d8902b
AobaIwaki <aobaiwaki@gmail.com>
2025-03-04 01:04:41 +0900

    check: storage upload

--- api/modules/upload_source.py
+++ api/modules/upload_source.py
@@ -1,16 +1,42 @@
 import subprocess
 
+from google.cloud import storage
 from models.job import Job
 
 
 def upload_source(job: Job) -> bool:
     job_id = job.job_id
-    source_path = f"tmp/{job_id}/source.mp4"
+    source_path = f"tmp/{job_id}/separated/htdemucs/source/vocals.wav"
 
-    subprocess.run(
-        ["yt-dlp", url, "-o", tmp_path],
-        capture_output=True,
-        text=True,
-    )
+    bucket_name = "musp"
+    destination_blob_name = f"{job_id}/vocals.wav"
+    
+    upload_blob(bucket_name, source_path, destination_blob_name)
 
     return True
+
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    # Optional: set a generation-match precondition to avoid potential race conditions
+    # and data corruptions. The request to upload is aborted if the object's
+    # generation number does not match your precondition. For a destination
+    # object that does not yet exist, set the if_generation_match precondition to 0.
+    # If the destination object already exists in your bucket, set instead a
+    # generation-match precondition using its generation number.
+    generation_match_precondition = 0
+
+    blob.upload_from_filename(
+        source_file_name, if_generation_match=generation_match_precondition
+    )
