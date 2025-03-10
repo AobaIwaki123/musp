3a27bb36c0c303e4f6f3ed2aac17ca237c2fcdc7
AobaIwaki <aobaiwaki@gmail.com>
2025-03-10 15:14:27 +0900

    fix: dev cors

--- view/src/client/api.ts
+++ view/src/client/api.ts
@@ -6,7 +6,4 @@ if (!publicApiUrl) {
   throw new Error("NEXT_PUBLIC_API_URL is required");
 }
 
-// export const api = createApiClient("http://localhost:8000");
-// export const api = createApiClient("http://100.92.146.108:8000");
-export const api = createApiClient("https://musp-api.shaoba.tech");
-// export const api = createApiClient(publicApiUrl);
+export const api = createApiClient(publicApiUrl);
