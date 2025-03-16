d3122176e61eeba1fd0a3ae186a0d5cd0cab4acb
AobaIwaki <aobaiwaki@gmail.com>
2025-03-16 22:12:52 +0900

    update: docker file

--- view/client/api.ts
+++ view/client/api.ts
@@ -1,13 +1,14 @@
 import { pluginToken } from '@zodios/plugins';
 import { createApiClient } from '@/client/client';
 
+
 // const apiKey = process.env.NEXT_PUBLIC_API_URL;
 // 
 // if (!publicApiUrl) {
 //   throw new Error('NEXT_PUBLIC_API_URL is required');
 // }
 
-export const api = createApiClient('http://100.92.146.108:8000');
+export const api = createApiClient('https://musp-dev-api.shaoba.tech');
 
 const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
