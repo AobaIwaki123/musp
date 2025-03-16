1aa98c76c9721124e697facd0450028a67d287d0
AobaIwaki <aobaiwaki@gmail.com>
2025-03-16 19:26:49 +0900

    update: login modal

--- view/components/Home/LoginModal/LoginModal.tsx
+++ view/components/Home/LoginModal/LoginModal.tsx
@@ -1,7 +1,29 @@
+import { Modal, Button, Center } from "@mantine/core";
 import { LoginButton } from "@/components/Buttons/LoginButton/LoginButton";
+import { useState } from "react";
 
-export function LoginModal () {
-  return (
-    <LoginButton />
-  )
+export function LoginModal() {
+	const [opened, setOpened] = useState(true);
+
+	return (
+		<>
+			{/* モーダル */}
+			<Modal
+				opened={opened}
+				onClose={() => {}}
+				withCloseButton={false} // ✨ 閉じるボタンを無効化
+				overlayProps={{
+					backgroundOpacity: 0.6, // ✨ 背景を暗くする
+					blur: 3, // ✨ 背景をぼかす
+				}}
+				centered // ✨ モーダルを中央に配置
+			>
+				<Center>
+					<LoginButton />
+				</Center>
+			</Modal>
+		</>
+	);
 }
+
+export default LoginModal;
