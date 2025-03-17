1a0cf22740451e5f61ecd55bf2d1f58a57c1eb86
AobaIwaki <aobaiwaki@gmail.com>
2025-03-17 19:15:10 +0900

    update: load env to get api url

--- view/client/api.ts
+++ view/client/api.ts
@@ -2,16 +2,16 @@ import { pluginToken } from '@zodios/plugins';
 import { createApiClient } from '@/client/client';
 
 
-// const apiURL = process.env.NEXT_PUBLIC_API_URL;
+const apiURL = process.env.NEXT_PUBLIC_API_URL;
 // 
-// if (!publicApiUrl) {
-//   throw new Error('NEXT_PUBLIC_API_URL is required');
-// }
+if (!apiURL) {
+  throw new Error('NEXT_PUBLIC_API_URL is required');
+}
 
-export const api = createApiClient('http://100.92.146.108:8000');
+export const api = createApiClient(apiURL);
 
-const apiKey = process.env.NEXT_PUBLIC_API_KEY;
-
+// const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+// 
 // if (!apiKey) {
 //   throw new Error('NEXT_PUBLIC_API_KEY is required');
 // }
