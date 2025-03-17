39f44e8d08b1b02363d9ff7808eb00ac258224a2
AobaIwaki <aobaiwaki@gmail.com>
2025-03-18 00:06:38 +0900

    add: gitignore

--- view/app/layout.tsx
+++ view/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }: { children: any }) {
 					<ColorSchemeScript />
 					<link rel="shortcut icon" href="/MuspIcon/MuspIcon-2.webp" />
 					<link rel="manifest" href="/manifest.json" />
-					<link rel="apple-touch-icon" href="/icon512_rounded.png" />
+					<link rel="apple-touch-icon" href="/MuspIcon/MuspIcon-2.webp" />
 					<meta
 						name="viewport"
 						content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
