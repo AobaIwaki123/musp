ee277d6890ed9b5fd1b1770845c9c100b6a96298
AobaIwaki <aobaiwaki@gmail.com>
2025-03-21 19:52:41 +0900

    fix: audio bug

--- view/components/Footer/Footer.tsx
+++ view/components/Footer/Footer.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { videoIDAtom } from "@/jotai/atom";
+import { wavURLAtom } from "@/jotai/atom";
 import { useAtom } from "jotai";
 import { AudioFooter } from "./AudioFooter/AudioFooter";
 import { DefaultFooter } from "./DefaultFooter/DefaultFooter";
 
 export function Footer() {
-	const [videoID] = useAtom(videoIDAtom);
+	const [wavURL] = useAtom(wavURLAtom);
 
 	return (
 		<>
 			<DefaultFooter />
-			{videoID && <AudioFooter key={videoID} />}
+			{wavURL && <AudioFooter key={wavURL} />}
 		</>
 	);
 }
