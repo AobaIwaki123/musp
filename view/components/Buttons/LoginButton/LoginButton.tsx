2a21faf83549d9cc9837f3e1f170f95ee9f3966c
AobaIwaki <aobaiwaki@gmail.com>
2026-02-18 22:10:48 +0900

    fix: firebaseのapikeyは公開してもOKだが、googleのアラートに引っかかるので環境変数に移動

--- view/components/Buttons/LoginButton/LoginButton.tsx
+++ view/components/Buttons/LoginButton/LoginButton.tsx
@@ -8,13 +8,13 @@ import { GoogleButton } from "../GoogleButton/GoogleButton";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDpsIs903Zk8xZtXxw4671DhVg4XAj8XxY",
-  authDomain: "musp-8f9bd.firebaseapp.com",
-  projectId: "musp-8f9bd",
-  storageBucket: "musp-8f9bd.firebasestorage.app",
-  messagingSenderId: "866327989325",
-  appId: "1:866327989325:web:982dd0d485f7c4a0fae004",
-  measurementId: "G-ZVGTSY36K1"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const provider = new GoogleAuthProvider();
